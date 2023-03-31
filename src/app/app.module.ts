3a8321af7cda63fd501e8f40820ca245bca6a4c6
Sowjanya <mrcraosowji@gmail.com>
2023-03-31 17:51:26 +0530

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
+import {HttpClientModule} from '@angular/common/http';
+import { EmployeeRegService } from './employee-reg.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeRegComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+ 
+
+  providers: [EmployeeRegService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
